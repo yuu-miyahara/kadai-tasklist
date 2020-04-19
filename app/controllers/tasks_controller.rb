140f37c036e312945226beefefc5c5decf12a78d
yuu.miyahara <miyaharayou1116@gmail.com>
2020-04-19 21:57:25 +0000

    test-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,13 +4,31 @@ class TasksController < ApplicationController
   end
   
   def create
-    
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'タスクが正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'タスクが投稿されませんでした'
+      render :new
+    end
   end
   
   def new
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'タスクが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが投稿されませんでした'
+      render :new
+    end
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
 
   def show
@@ -18,8 +36,29 @@ class TasksController < ApplicationController
   end
   
   def update
+    @task = Task.find(params[:id])
+
+    if @message.update(message_params)
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
+    end
   end
   
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_url
   end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
 end
