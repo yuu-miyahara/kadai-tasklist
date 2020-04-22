b23b08316e6a422484fa04970e9001a7f18687cb
yuu.miyahara <miyaharayou1116@gmail.com>
2020-04-22 03:50:37 +0000

    complete2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_064214) do
+ActiveRecord::Schema.define(version: 2020_04_22_024100) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
